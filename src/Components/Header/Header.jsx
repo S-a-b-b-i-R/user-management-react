dae94fb36e646b495b06b50f87fd8c2bea0135ca
S_a_b_b_i_R <sabbirssmc90@gmail.com>
2024-02-22 09:56:34 +0600

    minor changes

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+const Header = () => {
+    return (
+        <div className="hero p-10 rounded-lg bg-base-200">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">
+                        Please Select the data source
+                    </h1>
+                    {/* <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                        assumenda excepturi exercitationem quasi. In deleniti
+                        eaque aut repudiandae et a id nisi.
+                    </p>
+                    <button className="btn btn-primary">Get Started</button> */}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import Error from "../Components/Error/Error";
 import UserTable from "../Components/UserTable/UserTable";
 import MongoTable from "../Components/UserTable/MongoTable";
 import UserDetail from "../Pages/UserDetail";
+import Header from "../Components/Header/Header";
 
 const Routes = createBrowserRouter([
     {
@@ -11,6 +12,10 @@ const Routes = createBrowserRouter([
         element: <MainLayout />,
         errorElement: <Error />,
         children: [
+            {
+                path: "/",
+                element: <Header />,
+            },
             {
                 path: "/json",
                 element: <UserTable />,
